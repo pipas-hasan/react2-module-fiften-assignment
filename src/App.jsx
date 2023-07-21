ec95e53c771ede9facddb909c515713ff226d8e1
pipas-hasan <pipasrajbari@gmail.com>
2023-07-21 10:15:57 +0600

    complite

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,46 @@
 
-import './App.css'
+import React, { useState } from 'react';
+import "./App.css"
 
-function App() {
- 
+const TodoApp = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask]);
+      setNewTask('');
+    }
+  };
+
+  const removeTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
 
   return (
-   <div>
-    <h1>Hellow</h1>
-   </div>
-  )
-}
+    <div>
+      <h1>Todo List</h1>
+      <div>
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <p key={index}>
+            {task}
+            <button onClick={() => removeTask(index)}>Delete</button>
+          </p>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default App
+export default TodoApp;
